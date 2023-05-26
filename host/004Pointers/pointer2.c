dedf46d083754b9a873a69e2fa53fde10bdd19fb
Kevin Rivera <kevinriveraut@gmail.com>
2023-05-25 20:13:28 -0400

    Arithmetic with pointers

---
+++ host/004Pointers/pointer2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+long long int g_data = 0xFFFEABCD11112345;
+
+int main(void)
+{
+	int *pAddress = (int*)&g_data;
+	printf("Value of address %p\n", pAddress);
+	printf("Value at address %p is: %x\n", pAddress, *pAddress);
+
+	pAddress = pAddress + 1;
+
+	printf("Value of address %p\n", pAddress);
+	printf("Value at address %p is: %x\n", pAddress, *pAddress);
+
+	return 0;
+}
