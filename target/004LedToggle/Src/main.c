cc1a643f4b3728503e8fbbc6f3b3718fd4729da5
Kevin Rivera <kevinriveraut@gmail.com>
2023-05-31 20:52:50 -0400

    Managed to turn LD2 ON!

--- target/004LedToggle/Src/main.c
+++ target/004LedToggle/Src/main.c
@@ -25,8 +25,50 @@
 int main(void)
 {
 	// GPIOA: 0x4002 0000 - 0x4002 03FF
-	// GPIOA Port Mode register: 0x4002 0000 + 4
-	//
+
+	// GPIOA Port Mode register: 0x4002 0000
+	// GPIOA Port Mode Offset: 0x00
+	//	Equals to: 0x4002 0000
+
+	// GPIOA Pin 5 Mode:
+	//	Bits 11 and 10
+	//	General Purpose Output Mode = 01
+
+	// GPIOA Data Output Offset: 0x14
+	//	Equals to: 0x4002 0014
+
+	// GPIOA Pin 5 Output:
+	//	Bit 5
+	//	0 = Red
+	//	1 = Green
+
+	// RCC: 0x4002 3800 - 0x4002 3BFF
+	// RCC AHB1ENR Offset: 0x30
+	// 	Equals to: 0x4002 3830
+
+	// GPIOA RCC Enable
+	//	Bit 0
+
+	// Store register addresses
+	uint32_t *pGpioa_port_mode 		= (uint32_t*)0x40020000;
+	uint32_t *pGpioa_data_output 	= (uint32_t*)0x40020014;
+	uint32_t *pAhb1enr_rcc 			= (uint32_t*)0x40023830;
+
+	// Reset register values
+	//	*ahb1enr_rcc 					= (uint32_t)0x00000000;
+	//	*gpioa_port_mode 				= (uint32_t)0x0C000000;
+	//	*gpioa_data_output 				= (uint32_t)0x00000000;
+
+	// Activate bits
+	// rcc enable, bit 0 to 1
+	*pAhb1enr_rcc = *pAhb1enr_rcc | 0x00000001;
+	// gpioa port mode, bits 18, 19 to 01
+	*pGpioa_port_mode &= 0xFFFFBFFF;
+	*pGpioa_port_mode |= 0x00000400;
+	// gpio data output, bit 5 to 1
+	*pGpioa_data_output |= 0x00000020;
+
+
     /* Loop forever */
-	for(;;);
+	while(1);
 }
