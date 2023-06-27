ce96d711cd26732cea4c7302390163f46e577b5c
Kevin Rivera <kevinriveraut@gmail.com>
2023-06-26 21:00:52 -0400

    Finally the button interrupt is working. 命は美しい !

--- target/008Button_ISR/src/main.c
+++ target/008Button_ISR/src/main.c
@@ -9,11 +9,12 @@ uint32_t g_button_press_count =0;
 void button_init(void);
 
 uint32_t volatile *pEXTTIPendReg		= (uint32_t*) (0x40013C00 + 0x14);
-uint32_t volatile *pClkCtrlReg			= (uint32_t*) (0x40023800 + 0x30); //Bit 3 set to 1, for GPIOC
-uint32_t volatile *pClkCtrlRegApb2		= (uint32_t*) (0x40023800 + 0x44); //Looks good
+uint32_t volatile *pClkCtrlReg			= (uint32_t*) (0x40023800 + 0x30);
+uint32_t volatile *pClkCtrlRegApb2		= (uint32_t*) (0x40023800 + 0x44);
 uint32_t volatile *pEXTIMaskReg 		= (uint32_t*) (0x40013C00 + 0x00);
-uint32_t volatile *pEXTTIEdgeCtrlReg	= (uint32_t*) (0x40013C00 + 0x08); 
-uint32_t volatile *pNVICIRQEnReg 		= (uint32_t*) (0xE000E100 + 0x04); //Check offset for EXTICR4 = 0x14
+uint32_t volatile *pEXTTIEdgeCtrlReg	= (uint32_t*) (0x40013C00 + 0x08);
+uint32_t volatile *pSYSCFG_EXTICR4		= (uint32_t*) (0x40013800 + 0x14);	
+uint32_t volatile *pNVICIRQEnReg 		= (uint32_t*) (0xE000E100 + 0x04); //NVIX_ISER1 for EXTI15_10
 
 
 int main(void)
@@ -44,15 +45,17 @@ int main(void)
 
 void button_init(void)
 {
-    *pClkCtrlReg |= ( 1 << 2); //Enable RCC for GPIOC, bit 3 set to 1
-    *pClkCtrlRegApb2 |= ( 1 << 14); //SYSCFGEN, Sys Config Enable
+    *pClkCtrlReg |= ( 1 << 2); //Enable RCC for GPIOC, bit 2 set to 1
+    *pClkCtrlRegApb2 |= ( 1 << 14); //SYSCFGEN, bit 14
     *pEXTTIEdgeCtrlReg |= ( 1 << 13); //EXTI - RTSR 13
-    *pEXTIMaskReg |= ( 1 << 13); //Should it be 13?
-    *pNVICIRQEnReg |= ( 1 << 8); //1 to Bit 5, EXTI13
+    *pEXTIMaskReg |= ( 1 << 13);
+	*pSYSCFG_EXTICR4 &= ~(1 << 5); //For EXTI13
+    *pSYSCFG_EXTICR4 |= (1 << 5);
+    *pNVICIRQEnReg |= ( 1 << (40%32)); //1 to Bit 8, EXTI13
 }
 
 /* This is button interrupt handler*/
-void EXTI13_IRQHandler(void)
+void EXTI15_10_IRQHandler(void)
 {
     //Make this flag SET . if button pressed
     g_button_pressed = 1;
